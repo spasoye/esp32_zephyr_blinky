dfe2231d20b5d30a71376ea6bbd8e030aa6e3427
Ivan <ivan@spasoye.com>
2023-12-18 20:40:54 +0100

    WIP: zbus implementation

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/gpio.h>
+#include <zephyr/zbus/zbus.h>
 
 #define POLL_STACK_SIZE 500
 #define POLL_THREAD_PRIORITY 5
@@ -27,12 +28,20 @@ K_THREAD_STACK_DEFINE(react_thread_stack, REACT_STACK_SIZE);
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 static const struct gpio_dt_spec btn = GPIO_DT_SPEC_GET(BTN0_NODE, gpios);
 
+// zbus pin status
+struct pin_status
+{
+    bool value;
+    bool changed;
+};
+
 class PollClass{
 public:
-    PollClass(const struct gpio_dt_spec *btn)
+    PollClass(const struct gpio_dt_spec *btn, const struct zbus_channel *zbus_chan_arg)
     {
         printf("Initializing poll class\n");
-        
+
+        zbus_chan = zbus_chan_arg;
         btn_dev = btn;
 
         poll_thread_id = k_thread_create(&poll_thread_data,
@@ -41,7 +50,6 @@ public:
                                          PollClass::poll_thread_handler,
                                          this, NULL, NULL, POLL_THREAD_PRIORITY,
                                          0, K_NO_WAIT);
-
     }
 
     void start(void)
@@ -49,6 +57,9 @@ public:
         k_thread_join(&poll_thread_data,K_FOREVER);
     }
 private:
+    struct pin_status zbus_btn_state;
+    
+    const struct zbus_channel *zbus_chan;
     // Poll thread variables
     struct k_thread poll_thread_data;
     k_tid_t poll_thread_id;
@@ -76,19 +87,29 @@ private:
         bool btn_curr_state = gpio_pin_get_dt(btn_dev);
         btn_prev_state = gpio_pin_get_dt(btn_dev);
 
-        while (1) {
-            // ret = gpio_pin_toggle_dt(&led);
-            // if (ret < 0) {
-            //     return 0;
-            // }
-            
+        while (1) {           
             btn_curr_state = gpio_pin_get_dt(btn_dev);
             
+            zbus_btn_state.value = btn_curr_state;
+
+            // check if value is changed
             if (btn_curr_state != btn_prev_state)
             {
                 printf("Button state: %s\n", btn_curr_state ? "ON" : "OFF");
+                zbus_btn_state.changed = true;
+            }
+            else
+            {
+                zbus_btn_state.changed = false;
             }
         
+            int pub_ret = zbus_chan_pub(zbus_chan, &zbus_btn_state, K_MSEC(SLEEP_TIME_MS));
+
+            if (pub_ret != 0)
+            {
+                printf("Error publishing\n");
+            }
+
             btn_prev_state = btn_curr_state;
             k_msleep(SLEEP_TIME_MS);
         }
@@ -111,19 +132,29 @@ public:
 
         k_work_init_delayable(&led_timer, &ReactClass::blink_handler_bridge);
         k_work_reschedule(&led_timer, K_MSEC(timeout_ms));
-        
-        // react k_thread initialization
-        react_thread_id = k_thread_create(&react_thread_data,
-                                         react_thread_stack,
-                                         K_THREAD_STACK_SIZEOF(react_thread_stack),
-                                         ReactClass::react_thread_handler,
-                                         this, NULL, NULL, REACT_THREAD_PRIORITY,
-                                         0, K_NO_WAIT);
+
     }
 
     void start(void)
     {
         k_thread_join(&react_thread_data,K_FOREVER);
+    }
+
+    /**
+     * @brief  
+     * @note   
+     * @param  *chan: 
+     * @retval None
+     */
+    void zbus_listener_callback(const struct zbus_channel *chan)
+    {
+        const struct pin_status *pin = static_cast<const struct pin_status*>(zbus_chan_const_msg(chan));
+
+        printf("From listener\n");
+        
+        if (pin->changed){
+            timeout_ms = timeout_ms + 50;
+        }
     }
 
 private:
@@ -183,6 +214,23 @@ private:
     }
 };
 
+static void listener_callback_bridge(const struct zbus_channel *chan) {
+        ReactClass *react_obj = static_cast<ReactClass*>(zbus_chan_user_data(chan));
+        if (react_obj) {
+            react_obj->zbus_listener_callback(chan);
+        }
+    }
+
+ZBUS_CHAN_DEFINE(pin_status_chan,  /* Name */
+		 struct pin_status, /* Message type */
+
+		 NULL,                                 /* Validator */
+		 NULL,                                 /* User data */
+		 ZBUS_OBSERVERS(pin_lis),     /* observers */
+		 ZBUS_MSG_INIT(.value = false, .changed = true) /* Initial value */
+);
+
+ZBUS_LISTENER_DEFINE(pin_lis, listener_callback_bridge);
 
 int main(void)
 {
@@ -204,11 +252,18 @@ int main(void)
         return 0;
     }
 
-    PollClass check_gumbek(&btn);
+    ret = zbus_chan_add_obs(&pin_status_chan, &pin_lis, K_MSEC(200));
+    if (ret != 0)
+    {
+        printf("Add an observer to channel error");
+    }
+
+    PollClass check_gumbek(&btn, &pin_status_chan);
     ReactClass ledica(&led);
     
+
     check_gumbek.start();
-    ledica.start();
+
 
     while (true)
     {
